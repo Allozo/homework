5ccf1a1749f05edc73304743f64824ad35ff0056
allozo <allozo@yandex.ru>
2018-03-18 17:53:23 +0300

    Добавлен проект игры Пока есть только люди Реализована фабрика и строитель для юнитов фракции "люди" В Unit_Of_Men header создан класс Unit_of_men, от которого наследуются игровые юниты фракции человек.

---
+++ Project_game/TP_game/cmake-build-debug/CMakeFiles/TP_game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP_game.dir/main.cpp.o"
+  "TP_game.pdb"
+  "TP_game"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
